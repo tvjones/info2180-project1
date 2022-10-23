103e0247a96d309f7ad7bd4311efe1b7c364dc5c
Taye-Vaughn Jones <jonestaye@gmail.com>
2022-10-23 02:15:10 -0500

    display email

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
-/* Add your JavaScript to this file */
+window.onload = () =>{
+    
+      document.querySelector('button[type="submit"]').addEventListener("click",(e)=>{
+       
+        //prevent form submission
+        e.preventDefault()
+
+        //get email from field
+        const emailInputField = document.getElementById("email");
+        const email = emailInputField.value;
+
+        //get element to display message
+        const message = document.getElementsByClassName("message")[0]
+    
+
+        if(email.length===0){
+            message.innerHTML = "Please enter a valid email address."
+        }
+        else{
+            message.innerHTML = "Thank you! Your email address "+email+ " has been added to our mailing list!"
+        }
+
+      })
+      
+}
+
+
